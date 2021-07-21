95572d3d46007406e864d814f81066b7d21e41eb
ativasi <swgomes@hotmail.com>
2021-07-20 22:50:58 -0400

    Implementação do Repository - Insert - CRUD

--- Api.Data/Context/MyContext.cs
+++ Api.Data/Context/MyContext.cs
@@ -14,9 +14,19 @@ namespace Api.Data.Context
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            // COMANDO : Migrations
+            //     dotnet ef migrations add First_Migration
+            // COMANDO : Update no Banco
+            //     dotnet ef database update
             base.OnModelCreating(modelBuilder);
+
+
             // Entidade sendo carregada pela Estrutura MAP
             modelBuilder.Entity<UserEntity>(new UserMap().Configure);
+            // 1 - tableEntity
+            // 2 - Context
+            // 3 - MAP
+            // 4 - Migrations
 
         }
 
---
+++ Api.Data/Migrations/20210721021545_FirstMigration.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace data.Migrations
+{
+    public partial class FirstMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "User",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(60)", maxLength: 60, nullable: false),
+                    Email = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: true),
+                    CreateAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    UpdateAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true),
+                    DeleteAt = table.Column<DateTime>(type: "timestamp without time zone", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_User", x => x.Id);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_User_Email",
+                table: "User",
+                column: "Email",
+                unique: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "User");
+        }
+    }
+}
